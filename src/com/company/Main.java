a56bb447b4b3bc475c6c48d66e5241b4e8a581b5
HS211777 <HS211777@reigate.ac.uk>
2021-09-27 09:40:49 +0100

    commit branch2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,23 @@ public class Main {
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 
-    System.out.println("Which times table do you want?");
-    int userInput = input.nextInt();
-    int i;
-    for (i = 1; i < 11; i++){
-        System.out.println(i+" * "+userInput+" = "+(i*userInput));
-    }
+    //System.out.println("Which times table do you want?");
+    //int userInput = input.nextInt();
+    //int i;
+    //for (i = 1; i < 11; i++){
+        // System.out.println(i+" * "+userInput+" = "+(i*userInput));
+    //}
+        System.out.println("Enter a number");
+        int userInput = input.nextInt();
+        int j1 = 0;
+        int end = userInput;
+        for (int i = 0; i<userInput;i++){
+            for(int j = j1; j < end; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+            j1++;
+            end++;
+        }
     }
 }
