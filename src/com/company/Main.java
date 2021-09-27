01f7e0395a14e92818c179febfa1d19692edc34d
HS211777 <90605684+HS211777@users.noreply.github.com>
2021-09-27 09:43:02 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 
-    System.out.println("Which times table do you want?");
-    int userInput = input.nextInt();
-    int i;
-    for (i = 1; i < 11; i++){
-        System.out.println(i+" * "+userInput+" = "+(i*userInput));
-    }
+    //System.out.println("Which times table do you want?");
+    //int userInput = input.nextInt();
+    //int i;
+    //for (i = 1; i < 11; i++){
+        //System.out.println(i+" * "+userInput+" = "+(i*userInput));
+    //}
+	    for (int i = 0; i<10;i++){
+            for(int j = j1; j < 10; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+	    }
     }
 }
